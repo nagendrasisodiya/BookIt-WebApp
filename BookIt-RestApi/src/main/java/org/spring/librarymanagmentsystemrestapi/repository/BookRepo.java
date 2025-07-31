4128e20d8e1f2dd5b25195af5da60e8076ec701c
nagendrasisodiya <114593235+nagendrasisodiya@users.noreply.github.com>
2025-07-31 13:05:24 +0530

    Delete BookIt-RestApi directory

--- BookIt-RestApi/src/main/java/org/spring/librarymanagmentsystemrestapi/repository/BookRepo.java
+++
@@ -1,8 +0,0 @@
-package org.spring.librarymanagmentsystemrestapi.repository;
-
-import org.spring.librarymanagmentsystemrestapi.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepo extends JpaRepository<Book, Integer> {
-
-}
